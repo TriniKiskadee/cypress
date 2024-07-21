fbe9fe659173faeff7c0be9f237ffd6b75a6657d
TriniKiskadee <renaudsennon@gmail.com>
2024-07-21 04:04:30 -0400

    Add landing page

---
+++ src/app/(site)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface HomePageLayoutProps {
+    children: React.ReactNode;
+}
+
+const HomePageLayout = ({children}: HomePageLayoutProps) => {
+    return (
+        <main>
+            {children}
+        </main>
+    );
+};
+
+export default HomePageLayout;
